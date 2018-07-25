283ef7a89885bd41cac7ae477c902deb1e12eae5
Jacob <jacobmoyle@gmail.com>
2018-07-25 13:15:22 -0700

    reorder map new test" exit

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,11 +1,13 @@
 RSpec.describe TrainNavigator::Map do
 
   describe '#new' do
-    it 'requires user input' do
-      expect(lambda { TrainNavigator::Map.new }).to raise_error ArgumentError
+
+    it 'requires input' do
       map = TrainNavigator::Map.new('foo')
       expect(map.instance_of?(TrainNavigator::Map)).to be true
+      expect(lambda { TrainNavigator::Map.new }).to raise_error ArgumentError
     end
+
   end
   
 end
